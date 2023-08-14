a83ab7722e644556f677f795a10020a275455529
Maria Okatova <mariia.okatova@volkswagen.de>
2023-08-14 11:06:58 +0200

    refactor extra logic out of weather-for-the-day component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {TemperatureHistory} from "./model/temperature-history";
 import {TemperatureHistoryService, WeatherDto} from "./service/temperature-history.service";
+import {WeatherForTheDay} from "./model/weather-for-the-day";
 
 @Component({
   selector: 'app-root',
@@ -9,16 +10,34 @@ import {TemperatureHistoryService, WeatherDto} from "./service/temperature-histo
 })
 export class AppComponent {
   temperatureHistory: TemperatureHistory | undefined;
+  days: WeatherForTheDay[] = [];
   startDate = "2023-07-01";
   endDate = "2023-08-01";
   updateStartDate() {
     this.service.requestTemperature(this.startDate!, this.endDate!).subscribe((value) => {
       this.temperatureHistory = value;
+      this.moveDataToWeatherArray();
     });
   }
   constructor(public service: TemperatureHistoryService) {
     service.requestTemperature(this.startDate!, this.endDate!).subscribe((value) => {
       this.temperatureHistory = value;
+      this.moveDataToWeatherArray();
     });
+  }
+
+  private moveDataToWeatherArray() {
+    if (this.temperatureHistory) {
+      this.days.length = 0;
+      for (let i = 0; i < this.temperatureHistory.days.length; i += 24) {
+        let day: WeatherForTheDay =
+          {
+            days: this.temperatureHistory!.days.slice(i, i + 24),
+            temperatures: this.temperatureHistory!.temperatures.slice(i, i + 24),
+            rain: this.temperatureHistory!.rain.slice(i, i + 24)
+          };
+        this.days.push(day);
+      }
+    }
   }
 }
---
+++ frontend/src/app/model/weather-for-the-day.ts
@@ -0,0 +1,4 @@
+export class WeatherForTheDay {
+  constructor(public days: string[], public temperatures: string[], public  rain: string[]) {}
+
+}
